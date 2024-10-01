a86aa65e7974e8489f1e8284daa7c013fc2a2e17
Yarik Borisenko <73733920+DarkGreeZly@users.noreply.github.com>
2024-10-01 23:22:21 +0300

    add new paths

--- slots_game/slots/urls.py
+++ slots_game/slots/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.sign_in, name="login"),
     path('admin_page/', views.admin_page, name="admin_page"),
     path('admin_page/generate_url/', views.generate_url, name="generate_url"),
-    path('slots/<str:access_code>/', views.slots_web, name="slots"),
-    path('slots/<str:access_code>/slots_game/', views.slots_game, name="slots_game")
+    path('slots/<str:access_code>/<str:game_name>/', views.slots_web, name="slots"),
+    path('slots/<str:access_code>/<str:game_name>/slots_game/', views.slots_game, name="slots_game"),
+    path('slots/<str:access_code>/<str:game_name>/counter/', views.counter_data, name="counter"),
 ]
